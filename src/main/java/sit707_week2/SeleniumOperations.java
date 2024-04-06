31cb2159813b6d1202d80d94219dc8d1e5b1a876
Sandriya <sandriyafernandes10@gmail.com>
2024-04-06 22:12:33 +1100

    Update SeleniumOperations.java

--- src/main/java/sit707_week2/SeleniumOperations.java
+++ src/main/java/sit707_week2/SeleniumOperations.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.locators.RelativeLocator;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
